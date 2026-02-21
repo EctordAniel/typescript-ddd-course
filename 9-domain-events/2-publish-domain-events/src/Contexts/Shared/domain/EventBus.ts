bf0e9b5f993d25fac0d8836d899f5fcfb321cb33
Hectór Daniel Coronel <57757074+EctordAniel@users.noreply.github.com>
2026-02-21 19:54:26 -0300

    Delete 9-domain-events directory

--- 9-domain-events/2-publish-domain-events/src/Contexts/Shared/domain/EventBus.ts
+++
@@ -1,7 +0,0 @@
-import { DomainEvent } from './DomainEvent';
-import { DomainEventSubscriber } from './DomainEventSubscriber';
-
-export interface EventBus {
-  publish(events: Array<DomainEvent>): Promise<void>;
-  addSubscribers(subscribers: Array<DomainEventSubscriber<DomainEvent>>): void;
-}
