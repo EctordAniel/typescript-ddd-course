04466e5a8b0e906fe8d2ed8666a40be10994f329
Hectór Daniel Coronel <57757074+EctordAniel@users.noreply.github.com>
2026-02-21 19:52:19 -0300

    Delete 10-rabbitmq-message-broker directory

--- 10-rabbitmq-message-broker/2-publish-events-fallback/tests/Contexts/Shared/infrastructure/EventBus/__mocks__/RabbitMQConnectionDouble.ts
+++
@@ -1,8 +0,0 @@
-import { RabbitMQConnection } from '../../../../../../src/Contexts/Shared/infrastructure/EventBus/RabbitMQ/RabbitMqConnection';
-
-export class RabbitMQConnectionDouble extends RabbitMQConnection {
-
-  async publish(params: any): Promise<boolean> {
-    throw new Error();
-  }
-}
